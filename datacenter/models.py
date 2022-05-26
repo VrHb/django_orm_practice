8a6b0e83e7b31797dc786bba8838b07e6b889d0c
Victor Abdukarimov <viredmi@gmail.com>
2022-05-26 13:47:58 +0300

    Finished task

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import localtime
 
 
 class Passcard(models.Model):
@@ -28,3 +29,25 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+
+def get_duration(visiter: Visit) -> int:
+    if visiter.leaved_at:
+        delta = (visiter.leaved_at - visiter.entered_at).total_seconds()
+        return delta
+    delta = (localtime() - visiter.entered_at).total_seconds()
+    return int(delta)
+
+    
+def format_duration(duration: int) -> str:
+    duration_hours = int(duration // 3600)
+    duration_minutes = int(duration % 3600 // 60)
+    return f"{duration_hours}:{duration_minutes}:00"
+
+
+def is_visit_long(visit: Visit, minutes=60) -> bool:
+    visit_in_minutes = get_duration(visit) // 60
+    if visit_in_minutes > minutes:
+        return True
+    return False
+
--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,28 @@
-from datacenter.models import Passcard
+from datacenter.models import Passcard, is_visit_long
 from datacenter.models import Visit
 from django.shortcuts import render
 
+from datacenter.models import get_duration
+from datacenter.models import format_duration
+from datacenter.models import is_visit_long
+
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    passcard = Passcard.objects.get(passcode=passcode)
+    passcard_visits = [] 
+    visits_info = Visit.objects.filter(passcard=passcard)
+    for visit in visits_info:
+        visit_info = {
+            'entered_at': visit.entered_at,
+            'duration': format_duration(get_duration(visit)),
+            'is_strange': is_visit_long(visit)
+        }
+        passcard_visits.append(visit_info)
+    
+    
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': passcard_visits
     }
     return render(request, 'passcard_info.html', context)
+

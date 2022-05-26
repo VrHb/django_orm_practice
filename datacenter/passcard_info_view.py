8eeaa2bae550a861157048e2c3d02e89b1c1bbe0
Victor Abdukarimov <viredmi@gmail.com>
2022-05-26 16:32:59 +0300

    Fixed naming for ORM and list

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -9,20 +9,20 @@ from datacenter.models import is_visit_long
 
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.get(passcode=passcode)
-    passcard_visits = [] 
-    visits_info = Visit.objects.filter(passcard=passcard)
-    for visit in visits_info:
-        visit_info = {
+    serialized_visits = [] 
+    passcard_visits = Visit.objects.filter(passcard=passcard)
+    for visit in passcard_visits:
+        passcard_visit = {
             'entered_at': visit.entered_at,
             'duration': format_duration(get_duration(visit)),
             'is_strange': is_visit_long(visit)
         }
-        passcard_visits.append(visit_info)
+        serialized_visits.append(passcard_visit)
     
     
     context = {
         'passcard': passcard,
-        'this_passcard_visits': passcard_visits
+        'this_passcard_visits': serialized_visits
     }
     return render(request, 'passcard_info.html', context)
 
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,18 +6,18 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    non_closed_visits = []
-    visiters_in = Visit.objects.filter(leaved_at=None)
-    for visit in visiters_in:
+    serialized_visits = []
+    open_visits = Visit.objects.filter(leaved_at=None)
+    for visit in open_visits:
         visiter = {
             'who_entered': visit.passcard.owner_name,
             'entered_at': visit.entered_at,
             'duration': format_duration(get_duration(visit))
         }
-        non_closed_visits.append(visiter)
+        serialized_visits.append(visiter)
 
     context = {
-        'non_closed_visits': non_closed_visits,  
+        'non_closed_visits': serialized_visits,  
     }
     return render(request, 'storage_information.html', context)
 
